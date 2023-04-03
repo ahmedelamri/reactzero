05e80958f98fa0e6e52d48d3b38ff67d97e5c00f
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-04-03 19:46:13 +0200

    react basics 04 OK

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc == null ? (
@@ -13,16 +11,5 @@ function PokemonCard() {
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
